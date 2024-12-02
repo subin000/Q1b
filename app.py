2c78afc40c7dba2ae40e6dcf13ebabcb4e458190
Subin <subinsureshss123@gmail.com>
2024-12-02 11:36:01 +0530

    commit

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# Simple Streamlit app
+st.title("My Streamlit Web App")
+st.write("Hello, Streamlit!")
+st.write("Here is a random DataFrame:")
+
+# Display a random dataframe
+df = pd.DataFrame(np.random.randn(10, 2), columns=["A", "B"])
+st.write(df)
